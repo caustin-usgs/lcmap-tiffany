31b42fe35e9211594191333ec64e749108aa2679
clay austin <clay.austin.ctr@usgs.gov>
2018-11-27 10:33:26 -0600

    fun with spec

--- src/lcmap/tiffany/file.clj
+++ src/lcmap/tiffany/file.clj
@@ -1,7 +1,14 @@
 (ns lcmap.tiffany.file
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [clojure.spec.alpha :as spec]
+            [clojure.java.io :as io]
+            [lcmap.tiffany.validation :as validation]))
+
+(spec/def ::file_exists #(.exists (io/file %)))
+(spec/def ::file_path (spec/and string? ::file_exists))
 
 (defn read-json
   "Returns a lazy sequence"
   [infile]
-  (json/parse-stream (clojure.java.io/reader infile)))
+  (let [filepath (validation/check! ::file_path infile)]
+    (json/parse-stream (io/reader filepath))))
